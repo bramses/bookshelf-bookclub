ca249f0208e3783485f7fa4b8a424bc1dbd538b7
Bram Adams <3282661+bramses@users.noreply.github.com>
2025-12-11 22:16:28 -0500

    feat: force dynamic rendering for the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import ClientHome from '@/components/ClientHome';
 import { getBooks } from '@/lib/data';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const books = await getBooks();
   return <ClientHome books={books} />;
